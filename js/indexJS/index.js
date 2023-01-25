32a017fc980b65640186f9473b78777e79604e98
Raul Ramos <hdjsmraul@hotmail.com>
2023-01-25 20:43:41 -0300

    desfazendo alterações

--- js/indexJS/index.js
+++ js/indexJS/index.js
@@ -1,6 +1,5 @@
-const programas = document.querySelector('.programasOcultos')
-
 function mostrarProgramas() {
+    const programas = document.querySelector('.programasOcultos')
     if (programas.style.display == 'none') {
         programas.style.display = 'block'
     } else { programas.style.display = 'none' }
