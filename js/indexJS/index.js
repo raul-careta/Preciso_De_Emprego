9887efe97cd0767af5bac32eac38e42f531393ae
Raul Ramos <hdjsmraul@hotmail.com>
2023-01-25 20:31:16 -0300

    mudei nome da funcao js

--- js/indexJS/index.js
+++ js/indexJS/index.js
@@ -1,6 +1,5 @@
 const programas = document.querySelector('.programasOcultos')
 
-//quando clica em(Meus Programas)mostra os programas ocultos
 function mostrarProgramas() {
     if (programas.style.display == 'none') {
         programas.style.display = 'block'
