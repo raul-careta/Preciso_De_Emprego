d3db6e30063b7caaaf96b17fa2a400fbb9bed293
Raul Ramos <hdjsmraul@hotmail.com>
2023-01-30 18:40:49 -0300

    arrumando responsividade

--- js/index.js
+++
@@ -1,6 +0,0 @@
-function mostrarProgramas() {
-    const programas = document.querySelector('.programasOcultos')
-    if (programas.style.display == 'none') {
-        programas.style.display = 'block'
-    } else { programas.style.display = 'none' }
-}
